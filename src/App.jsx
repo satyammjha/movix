3c90f934e3337740aeac2b2adda912e919681f34
satyammjha <satyammjha0@gmail.com>
2024-01-02 21:06:38 +0530

    api setup and testing results in console

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+
+function App() {
+
+  let style = {
+
+    color: "red",
+  }
+
+
+  useEffect(() => {
+
+    apitest();
+
+  }, [])
+
+
+  const apitest = () => {
+
+    fetchDataFromApi('/movie/popular').then((res) => {
+
+      console.log(res);
+
+    })
+
+  }
+
+
+
+  return <h1 style={style}>Sada shiv sarvada shiv</h1>
+
+
+}
+
+
+
+export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+
+const headers = {
+    Authorization: `Bearer ` + TMDB_TOKEN, 
+};
+
+export const fetchDataFromApi = async (url, params) => {
+    try {
+        const response = await axios.get(BASE_URL + url, { headers, params });
+        return response.data; 
+    } catch (error) {
+        console.error(error);
+        throw error; 
+    }
+};
