002fcf504aed023b66692069650c32d0e7fac009
satyammjha <satyammjha0@gmail.com>
2024-01-03 22:12:10 +0530

    modified app.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,15 +5,15 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
-    Authorization: `Bearer ` + TMDB_TOKEN, 
+    Authorization: `Bearer ` + TMDB_TOKEN,
 };
 
 export const fetchDataFromApi = async (url, params) => {
     try {
         const response = await axios.get(BASE_URL + url, { headers, params });
-        return response.data; 
+        return response.data;
     } catch (error) {
         console.error(error);
-        throw error; 
+        throw error;
     }
 };
